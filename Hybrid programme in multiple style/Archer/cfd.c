270763e427f7feffdd2d423bb2aab61526462f97
Xinwen Zou <32584784+yvonnezou@users.noreply.github.com>
2018-08-23 14:33:08 +0100

    Update cfd.c

--- Hybrid programme in multiple style/Archer/cfd.c
+++ Hybrid programme in multiple style/Archer/cfd.c
@@ -304,9 +304,7 @@ int main(int argc, char **argv)
    * All threads are able to see it.
    *
   **/
-#pragma omp parallel default (none) shared(time_iter_thread,time_comm_thread,time_comp_thread,\
-zettmp,psitmp,zet,psi,n,numiter,irrotational,lm,\
-re,comm,printfreq,rank,checkerr) private(i,j)
+#pragma omp parallel default (none) shared(time_iter_thread,time_comm_thread,time_comp_thread,zettmp,psitmp,zet,psi,n,numiter,irrotational,lm,re,comm,printfreq,rank,checkerr) private(i,j)
   {
   /**
    *
